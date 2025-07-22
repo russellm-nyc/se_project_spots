024a8d841316057b25c35a7a48461848a5a478f1
russellm <russell@russellm.com>
2025-07-22 07:32:51 -0400

    update index.html and index.css

--- scripts/index.js
+++ scripts/index.js
@@ -27,27 +27,64 @@ const initialCards = [
 const profileOpenBtn = document.querySelector(".profile__edit-btn");
 const profileModal = document.querySelector("#edit-profile-modal");
 const profileCloseBtn = profileModal.querySelector(".modal__close-btn");
+const profileFormEl = profileModal.querySelector(".modal__form");
+
+const profileTitleEl = document.querySelector(".profile__title");
+const profileSubtitleEl = document.querySelector(".profile__subtitle");
+const profileNameInput = profileModal.querySelector("#profile-name-input");
+const profileDescriptionInput = profileModal.querySelector(
+  "#profile-description-input"
+);
 
 profileOpenBtn.addEventListener("click", function () {
-  profileModal.setAttribute("style", "visibility: visible");
-  //console.log("Edit button clicked");
+  profileModal.classList.add("modal_is-opened");
+  profileNameInput.value = profileTitleEl.textContent;
+  profileDescriptionInput.value = profileSubtitleEl.textContent;
 });
 profileCloseBtn.addEventListener("click", function () {
-  profileModal.setAttribute("style", "visibility: hidden");
-  //console.log("Edit button clicked");
+  profileModal.classList.remove("modal_is-opened");
 });
 
 const postOpenBtn = document.querySelector(".profile__plus-btn");
 const postModal = document.querySelector("#new-post-modal");
 const postCloseBtn = postModal.querySelector(".modal__close-btn");
+const postFormEl = postModal.querySelector(".modal__form");
+
+const postLinkInput = postModal.querySelector("#post-link-input");
+const postCaptionInput = postModal.querySelector("#post-caption-input");
 
 postOpenBtn.addEventListener("click", function () {
-  postModal.setAttribute("style", "visibility: visible");
+  postModal.classList.add("modal_is-opened");
+  postLinkInput.value = "";
+  postCaptionInput.value = "";
 });
+
 postCloseBtn.addEventListener("click", function () {
-  postModal.setAttribute("style", "visibility: hidden");
+  postModal.classList.remove("modal_is-opened");
 });
 
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileTitleEl.textContent = profileNameInput.value;
+  profileSubtitleEl.textContent = profileDescriptionInput.value;
+  // Close the modal.
+  profileModal.classList.remove("modal_is-opened");
+}
+
+// Set the submit listener.
+profileFormEl.addEventListener("submit", handleProfileFormSubmit);
+postFormEl.addEventListener("submit", handleAddCardSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(postLinkInput.value);
+  console.log(postCaptionInput.value);
+  // Close the modal.
+  postModal.classList.remove("modal_is-opened");
+}
